879a8ec2b181091c865614580c61fe310ca2164b
Ilya Kaplan <ik@alphaguilty.io>
2023-10-17 10:35:09 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Button from "./Button";
+import Open from "./Open";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<></>} />
+        <Route path="open" element={<Open />} />
+        <Route path="button" element={<Button />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Open.tsx
@@ -0,0 +1,17 @@
+import "./open.css";
+
+const Open = () => {
+  return (
+    <div id="iframeContainer" className="open">
+      <iframe
+        title="Iframe"
+        src="https://alphaguilty.io/iframe/alphaguilty-onboarding"
+        allow="scripts popups; clipboard-read; clipboard-write"
+        id="alphaguilty-iframe"
+        className="iframe"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Open;
